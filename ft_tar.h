2098e1179f50551d323662d740f5a4494893e4a7
Joey Chung <joeychung960@gmail.com>
2018-01-27 09:57:56 -0800

    I think I'm almost done with ft_archive. Just need to separate the archive() function from the buffer write which is in function buf_write

--- ft_tar.h
+++ ft_tar.h
@@ -6,7 +6,7 @@
 /*   By: jchung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 08:15:55 by jchung            #+#    #+#             */
-/*   Updated: 2018/01/27 08:36:34 by jchung           ###   ########.fr       */
+/*   Updated: 2018/01/27 09:10:11 by jchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,15 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+# define BLOCK_SIZE 512
+
 typedef struct		s_header {
 	char			name[100];
+	char			linkname[100];
 	char			size[12];
-	char			filler[400];
+	char			filler[300];
 }					t_header;
+
+void				ft_archive(int tarfd, char *filename, char *nextfile);
 
 #endif
--- main.c
+++ main.c
@@ -6,45 +6,41 @@
 /*   By: jchung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 08:33:21 by jchung            #+#    #+#             */
-/*   Updated: 2018/01/27 08:35:22 by jchung           ###   ########.fr       */
+/*   Updated: 2018/01/27 09:56:45 by jchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tar.h"
 
-typedef struct		s_header {
-	char			name[100];
-	char			size[12];
-	char			linkname[100];
-	char			filler[300];
-}					t_header;
-
-int					main(void)
+int				main(void)
 {
-	int				i;
-	int				fo_tar;
+
+	int			i;
+	int			fd_tar;
 	
 	if (argc > 2)
 	{
 		i = 1;
 		
-		fo_tar = open((const char *)argv[1], O_CREAT);//Assign fd
+		fd_tar = open((const char *)argv[1], O_CREAT);//Assign fd
 		
 		//Prepare to "archive" files by writing to fd per file;
 		while (argv[++i])
 		{
-			ft_archive(fo_tar, argv[i]);
+			argv[i + 1] != 0 ?
+				ft_archive(fd_tar, argv[i], argv[i + 1]) :
+				ft_archive(fd_tar, argv[i], NULL);
 		}
 	}
 	else
 		printf("usage: ft_archive tar_name file1 ...\n");
-	close(fo_tar);
+	close(fd_tar);
 	return (0);
 }
 
-t_block				*init(char *name)
+t_block			*init(char *name)
 {
-	t_block			*newblock;
+	t_block		*newblock;
 	
 	newblock = (t_block *)malloc(sizeof(t_block));
 	if (!newblock)
@@ -55,12 +51,59 @@ t_block				*init(char *name)
 	return (newblock);
 }
 
-void				ft_archive(int tarfd, char *filename)
+void			ft_archive(int tarfd, char *filename, char *nextfile)
 {
-	t_block			*block;
-	int				fo_file;
+	t_header	*header;
+	char		*arr;
+	char		*itr;
+	int			fd_file;
+	int			arrsize;
+	int			size; //Size of file, convert to string later in header
+	int			rdbk;
 	
-	block = init(filename);
-	fo_file = open((const char *)filename, O_RDONLY);
+	//Init the header of file
+	header = (t_header *){filename, nextfile, 0, 0};
+
+	fd_file = open((const char *)filename, O_RDONLY);
 	//Prepare to read, possibly with get_next_line
+	size = 0;
+	arrsize = BLOCK_SIZE;
+	itr = arr;
+	realloc(arr, arrsize); //Defaults to malloc
+
+	while ((rdbk = read(fd_file, itr, BLOCK_SIZE)))
+	{
+		size += rdbk;
+		if (size == arrsize)
+			realloc(arr, (arrsize += BLOCK_SIZE));
+		itr += rdbk;
+	}
+	header->size = size;
+	memset(itr, 0, arrsize - itr);
+	close(fd_file);
+}
+
+char			*write_buf(t_header *header, int fd)
+{
+	char		*buf;
+	char		*itr;
+	int			bufsize;
+	int			size;
+	int			rdbk;
+	
+	itr = buf;
+	bufsize = BLOCK_SIZE;
+	size = 0;
+	realloc(buf, bufsize);
+
+	while ((rdbk = read(fd, itr, BLOCK_SIZE)))
+	{
+		size += rdbk;
+		if (size == arrsize)
+			realloc(buf, (bufsize += BLOCK_SIZE));
+		itr += rdbk;
+	}
+	header->size = size;
+	memset(itr, 0, arrsize - itr);
+	return (buf);
 }
